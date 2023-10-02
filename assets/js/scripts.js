f45fb7c5207b9d0a7455306b5eba078c7703fd68
Your Name <you@example.com>
2023-10-02 19:58:29 -0300

    continuação aula

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,22 @@
 console.log("Olá Mundo!");
+// Função simples
+function digaOi() {
+  alert("oi");
+}
+
+// Função anônima - não tem nome
+const digaOi2 = () => {
+  alert("oi2");
+};
+
+// Função anônima - de retorno
+const digaOi3 = () => "oi3";
+
+// Função anônima - de retorno com parâmetro
+const digaOi4 = (nome) => "oi4 " + nome;
+
+// Função anônima - de retorno com parâmetro utilizando
+// template string com crase
+const digaOi5 = (nome) => `oi5 ${nome}`;
+
+alert(digaOi5("Juana Feliz"));
