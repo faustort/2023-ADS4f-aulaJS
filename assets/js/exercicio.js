a0abe7f85526198a8b5b25c66d98fa260ab3b23d
Your Name <you@example.com>
2023-10-02 21:34:45 -0300

    exercicio

--- assets/js/exercicio.js
+++ assets/js/exercicio.js
@@ -1,3 +1,14 @@
 // https://dummyjson.com/docs/posts
 // criar uma estrutura de publicação de notícias
 // para o retorno do Json acima
+
+/*
+https://getbootstrap.com/docs/5.3/components/card/
+
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Título</h5>
+    <p class="card-text">Descrição da notícia.</p>
+  </div>
+</div>
+*/
