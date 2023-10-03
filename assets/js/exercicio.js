58177bd9384514494ed9cf4b7d9a6248677c0fd7
Your Name <you@example.com>
2023-10-02 21:57:05 -0300

    ex resolvido

--- assets/js/exercicio.js
+++ assets/js/exercicio.js
@@ -12,3 +12,28 @@ https://getbootstrap.com/docs/5.3/components/card/
   </div>
 </div>
 */
+
+// primeiro precisamos buscar
+const buscaPosts = fetch("https://dummyjson.com/posts")
+  // then transformar em json
+  .then((response) => response.json())
+  // then receber os dados em json e ver onde vamos fazer o forEach
+  .then((data) => {
+    const posts = data.posts;
+    posts.forEach((post) => {
+      const card = document.createElement("div");
+      const cardBody = document.createElement("div");
+      const titulo = document.createElement("h5");
+      const texto = document.createElement("p");
+      card.classList.add("card");
+      cardBody.classList.add("card-body");
+      titulo.classList.add("card-title");
+      texto.classList.add("card-text");
+      titulo.innerText = post.title;
+      texto.innerText = post.description;
+      cardBody.append(titulo);
+      cardBody.append(texto);
+      card.append(cardBody);
+      document.body.append(card);
+    });
+  });
